killbill/killbill-analytics-plugin

commit 7db681f9cbd564f4f6e76cf287403d5668817812

reports.js: pass apiKey/apiSecret to Kill Bill
Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

src/main/resources/static/javascript/reports.js
@@ -85,21 +85,17 @@ Reports.prototype.hasReport = function(val) {
 
 Reports.prototype.availableReports = function(callback) {
     var url = this.buildBaseURL('/plugins/killbill-analytics/reports');
-    $.get(url, function(allReports) { callback(allReports); }, 'json');
+    this.doGet(url, function(allReports) { callback(allReports); });
 }
 
 Reports.prototype.getDataForReport = function(position, callback) {
     var url = this.buildDataURL(position);
 
-    return $.ajax({
-        type: 'GET',
-        contentType: 'application/json',
-        dataType: 'json',
-        url: url
-    }).done(function(data) {
+    return this.doGet(url,
+            function(data) {
                 callback(position, data);
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
+            },
+            function(jqXHR, textStatus, errorThrown) {
                 if (jqXHR.responseText) {
                     try {
                         errors = $.parseJSON(jqXHR.responseText);
@@ -202,4 +198,17 @@ Reports.prototype.buildDataURL = function(position, format) {
 
 Reports.prototype.buildBaseURL = function(path) {
     return this.protocol + '://' + this.host + ':' + this.port + path;
+}
+
+Reports.prototype.doGet = function(url, doneCallback, failCallback) {
+    var apiKey = $.url().param('apiKey') || 'bob';
+    var apiSecret = $.url().param('apiSecret') || 'lazar';
+
+    $.ajax({
+        type: 'GET',
+        contentType: 'application/json',
+        headers: { 'X-Killbill-ApiKey': apiKey, 'X-Killbill-ApiSecret': apiSecret },
+        dataType: 'json',
+        url: url
+    }).done(doneCallback).fail(failCallback);
 }

commit 34fe099fe58584eead24ea4fd1f3b9bbde4869ca

reports.js: fix regression introduced by 7db681f9cbd564f4f6e76cf287403d5668817812
Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

src/main/resources/static/javascript/reports.js
@@ -204,7 +204,7 @@ Reports.prototype.doGet = function(url, doneCallback, failCallback) {
     var apiKey = $.url().param('apiKey') || 'bob';
     var apiSecret = $.url().param('apiSecret') || 'lazar';
 
-    $.ajax({
+    return $.ajax({
         type: 'GET',
         contentType: 'application/json',
         headers: { 'X-Killbill-ApiKey': apiKey, 'X-Killbill-ApiSecret': apiSecret },