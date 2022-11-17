e9a14947d306bcadba3cfe4bd2f00f6e2ea8127e
xsalefter <xsalefter@gmail.com>
2022-11-17 10:06:07 +0700

    add missing parameter to subscriptionApi.getSubscriptionForEntitlementId()

--- src/main/java/org/killbill/billing/plugin/analytics/dao/factory/BusinessFactoryBase.java
+++ src/main/java/org/killbill/billing/plugin/analytics/dao/factory/BusinessFactoryBase.java
@@ -226,7 +226,7 @@ public abstract class BusinessFactoryBase {
 
         try {
             final List<SubscriptionBundle> bundles = subscriptionApi.getSubscriptionBundlesForExternalKey(bundleExternalKey, context);
-            if (bundles.size() == 0) {
+            if (bundles.isEmpty()) {
                 throw new AnalyticsRefreshException("Unable to retrieve latest bundle for bundle external key " + bundleExternalKey);
             }
             return bundles.get(bundles.size() - 1);
@@ -240,7 +240,7 @@ public abstract class BusinessFactoryBase {
         final SubscriptionApi subscriptionApi = getSubscriptionApi();
 
         try {
-            return subscriptionApi.getSubscriptionForEntitlementId(subscriptionId, context);
+            return subscriptionApi.getSubscriptionForEntitlementId(subscriptionId, false, context);
         } catch (final SubscriptionApiException e) {
             logger.warn("Error retrieving subscription for id {}", subscriptionId, e);
             throw new AnalyticsRefreshException(e);
